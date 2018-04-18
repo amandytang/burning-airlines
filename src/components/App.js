1defabf389de340d5be2c65cd9db9deb1fc00a70
Joseph Ocampo <josephocampo@MacBook-Pro-6.local>
2018-04-18 17:43:41 +1000

    added SelectSeat component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { PureComponent as Component } from 'react';
 import FlightBooker from './FlightBooker';
+import SelectSeat from './SelectSeat';
 
 class App extends Component {
   constructor(props) {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <img className="header" src="https://i.imgur.com/w6NB0jx.png" alt="Hero" />
         <div><FlightBooker /></div>
+        <div><SelectSeat /></div>
       </div>
     );
   }
---
+++ src/components/SelectSeat.js
@@ -0,0 +1,72 @@
+import React, { PureComponent as Component} from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3001/flights/1.json'
+
+// get the entire seatmap data from the server via axios .
+// display the data in seatMap
+// show which seats are taken and which are not live via polling
+// onclick user can select seat id. show selected seat in the dom.
+// when user clicks select seat. chosen seat id is sent to the database as not available anymore.
+
+// function hello(){
+//   console.log('isthisworking')
+// }
+//
+class SeatMap extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      seats: Array.from({length: 40}, (x,i) => i+1),
+      selectedSeat: ''}
+    this._handleChange = this._handleChange.bind(this);
+   }
+
+   _handleChange(e){
+    alert(e.currentTarget.id);
+    this.setState({selectedSeat: e.currentTarget.id });
+ }
+
+
+    render() {
+      return(
+        <div className="seatMap">
+            {this.state.seats.map((s) => <div onClick={this._handleChange} id={s} className="seat"><p>{s}</p></div>)}
+        </div>
+      );
+    }
+
+}
+
+// function SeatMap(props){
+//   return(
+//     <section className="seatMap">
+//         {props.seat.map((s) => <div id={s} className="seat"><p>{s}</p></div>)}
+//     </section>
+//   );
+// }
+
+class SelectSeat extends Component {
+  constructor(props){
+    super(props);
+    this.state = {seats: Array.from({length: 40}, (x,i) => i+1) };
+
+  //   //Polling
+  //   const fetchSecrets = () => { // fat arrow functions do not break the conenction to this
+  //     axios.get(SERVER_URL).then(results => this.setState({secrets: results.data}))
+  //     setTimeout(fetchSecrets, 4000); // recursion
+  //   }
+  //
+  // fetchSecrets();
+}
+
+  render(){
+    return(
+      <div>
+        <SeatMap seat={this.state.seats}/>
+      </div>
+    );
+  }
+}
+
+export default SelectSeat;
